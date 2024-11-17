dbae5d05bb423a437161fef0c7c242bc0910c3a2
Pascal Laurent <plaurentai@gmail.com>
2024-11-17 06:54:41 -0500

    getting input from the user

---
+++ projects/guessing_game/target/debug/deps/guessing_game-e5108777cc15123f.d
@@ -0,0 +1,5 @@
+/Users/jude/Data/dev/rust/playing-with-rust/projects/guessing_game/target/debug/deps/guessing_game-e5108777cc15123f: src/main.rs
+
+/Users/jude/Data/dev/rust/playing-with-rust/projects/guessing_game/target/debug/deps/guessing_game-e5108777cc15123f.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/jude/Data/dev/rust/playing-with-rust/projects/guessing_game/target/debug/guessing_game: /Users/jude/Data/dev/rust/playing-with-rust/projects/guessing_game/src/main.rs
